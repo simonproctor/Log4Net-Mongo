9d9fe99e6502d3e4358b5beb4aaa6cd14124432f
Simon Proctor <simon.proctor@brother.co.uk>
2014-12-08 14:31:24 +0000

    Initial commit

---
+++ src/MongoAppender.Tests/MongoDbAppenderTests.cs
@@ -0,0 +1,170 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MongoDbAppenderTests.cs" company="Simon Proctor">
+//   Copyright (c) 2014 Simon Proctor
+//   
+//   Permission is hereby granted, free of charge, to any person obtaining a copy
+//   of this software and associated documentation files (the "Software"), to deal
+//   in the Software without restriction, including without limitation the rights
+//   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//   copies of the Software, and to permit persons to whom the Software is
+//   furnished to do so, subject to the following conditions:
+//   
+//   The above copyright notice and this permission notice shall be included in
+//   all copies or substantial portions of the Software.
+//   
+//   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//   THE SOFTWARE.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace MongoAppender.Tests
+{
+    using System;
+    using System.Configuration;
+    using System.Linq;
+    using System.Threading;
+    using System.Xml;
+
+    using log4net;
+    using log4net.Config;
+
+    using MongoDB.Bson;
+    using MongoDB.Driver;
+    using MongoDB.Driver.Builders;
+
+    using NUnit.Framework;
+
+    using SharpTestsEx;
+
+    /// <summary>
+    /// Integration test fixture for recording and query for log entries in the test mongo 
+    /// database.
+    /// </summary>
+    [TestFixture]
+    [Category("Integration")]
+    public class MongoDbAppenderTests
+    {
+        /// <summary>
+        /// The log4net handle
+        /// </summary>
+        private static readonly ILog Log = LogManager.GetLogger(typeof(MongoDbAppenderTests));
+
+        /// <summary>
+        /// The Mongo collection to query for log records
+        /// </summary>
+        private MongoCollection collection;
+
+        /// <summary>
+        /// Initializes static members of the <see cref="MongoDbAppenderTests"/> class. 
+        /// Currently this is the log4net setup.
+        /// </summary>
+        static MongoDbAppenderTests()
+        {
+            XmlElement element = (XmlElement)ConfigurationManager.GetSection("log4net");
+            DOMConfigurator.Configure(element);
+        }
+
+        /// <summary>
+        /// Sets up access to the default logging collection to query for logs
+        /// post any log call.
+        /// </summary>
+        [SetUp]
+        public void SetUp()
+        {
+            MongoClient client = new MongoClient("mongodb://localhost");
+
+            MongoServer conn = client.GetServer();
+            MongoDatabase db = conn.GetDatabase("log4net");
+            this.collection = db.GetCollection("logs");
+        }
+
+        /// <summary>
+        /// Tests that the timestamp has been recorded
+        /// </summary>
+        [Test]
+        public void TimestampRecordedTest()
+        {
+            Log.Info("a log");
+
+            BsonDocument doc = this.collection.FindOneAs<BsonDocument>();
+            doc.GetElement("timestamp").Value.Should().Be.OfType<BsonDateTime>();
+        }
+
+        /// <summary>
+        /// Tests that multiple log statements are accurately logged through the buffer
+        /// </summary>
+        [Test]
+        public void BufferedLogsRecordedTest()
+        {
+            Log.Info("a log 1");
+            Log.Info("a log 2");
+            Log.Info("a log 3");
+            Log.Info("a log 4");
+            Log.Info("a log 5");
+
+            SortByBuilder sbb = new SortByBuilder();
+            sbb.Descending("_id");
+
+            var allDocs = this.collection.FindAllAs<BsonDocument>().SetSortOrder(sbb).SetLimit(5);
+
+            BsonDocument doc = allDocs.First();
+            Assert.AreEqual(doc.GetElement("message").Value.AsString, "a log 5");
+        }
+
+        /// <summary>
+        /// Tests that the log level has been recorded
+        /// </summary>
+        [Test]
+        public void LogLevelRecordedTest()
+        {
+            Log.Info("a log");
+
+            var doc = this.collection.FindOneAs<BsonDocument>();
+            doc.GetElement("level").Value.Should().Be.OfType<BsonString>();
+            doc.GetElement("level").Value.AsString.Should().Be.EqualTo("INFO");
+        }
+
+        /// <summary>
+        /// Tests that the log thread has been recorded
+        /// </summary>
+        [Test]
+        public void LogThreadRecordedTest()
+        {
+            Log.Info("a log");
+
+            BsonDocument doc = this.collection.FindOneAs<BsonDocument>();
+            doc.GetElement("thread").Value.Should().Be.OfType<BsonString>();
+            doc.GetElement("thread").Value.AsString.Should().Be.EqualTo(Thread.CurrentThread.Name);
+        }
+
+        /// <summary>
+        /// Tests that the exception has been logged.
+        /// </summary>
+        [Test]
+        public void LogExceptionRecorded()
+        {
+            try
+            {
+                throw new ApplicationException("BOOM");
+            }
+            catch (Exception e)
+            {
+                Log.Fatal("a log", e);
+            }
+
+            SortByBuilder sbb = new SortByBuilder();
+            sbb.Descending("_id");
+
+            var allDocs = this.collection.FindAllAs<BsonDocument>().SetSortOrder(sbb).SetLimit(1);
+            
+            BsonDocument doc = allDocs.First();
+            doc.GetElement("exception").Value.Should().Be.OfType<BsonString>();
+            doc.GetElement("exception").Value.AsString.Should().Contain("BOOM");
+        }
+    }
+}
---
+++ src/MongoAppender/MongoDbAppender.cs
@@ -0,0 +1,198 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MongoDbAppender.cs" company="Simon Proctor">
+//   Copyright (c) 2014 Simon Proctor
+//   
+//   Permission is hereby granted, free of charge, to any person obtaining a copy
+//   of this software and associated documentation files (the "Software"), to deal
+//   in the Software without restriction, including without limitation the rights
+//   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//   copies of the Software, and to permit persons to whom the Software is
+//   furnished to do so, subject to the following conditions:
+//   
+//   The above copyright notice and this permission notice shall be included in
+//   all copies or substantial portions of the Software.
+//   
+//   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//   THE SOFTWARE.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace MongoAppender
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Configuration;
+
+    using log4net.Appender;
+    using log4net.Core;
+
+    using MongoDB.Bson;
+    using MongoDB.Driver;
+
+    /// <summary>
+    /// The log4net appender. Saves log records to a mongo database.
+    /// </summary>
+    public class MongoDbAppender : BufferingAppenderSkeleton
+	{
+        /// <summary>
+        /// The list of log fields to save. Initialised from the log4net configuration
+        /// </summary>
+		private readonly List<MongoAppenderField> fields = new List<MongoAppenderField>();
+
+        /// <summary>
+        /// Gets or sets the MongoDB database connection in the format:
+        /// mongodb://[username:password@]host1[:port1][,host2[:port2],...[,hostN[:portN]]][/[database][?options]]
+        /// If no database specified, default to "log4net"
+        /// </summary>
+        public String ConnectionString { get; set; }
+
+		/// <summary>
+		/// Gets or sets the connectionString name to use in the connectionStrings section of the *.config file
+		/// If not specified or connectionString name does not exist will use ConnectionString value
+		/// </summary>
+		public String ConnectionStringName { get; set; }
+
+		/// <summary>
+		/// Gets or sets the name of the collection in the database. Defaults to "logs"
+		/// </summary>
+		public String CollectionName { get; set; }
+
+        /// <summary>
+        /// Gets or sets Mongo collection to write to. Initialised when the appender is activated
+        /// </summary>
+        private MongoCollection Collection { get; set; }
+
+        /// <summary>
+        /// Adds an entry from the config to the list of fields to log
+        /// </summary>
+        /// <param name="field">The field to log</param>
+		public void AddField(MongoAppenderField field)
+		{
+			this.fields.Add(field);
+		}
+
+        /// <summary>
+        /// Initialize the appender based on the options set
+        /// </summary>
+        /// <remarks>
+        /// <para>
+        /// This is part of the <see cref="IOptionHandler"/> delayed object
+        /// activation scheme. The <see cref="ActivateOptions"/> method must 
+        /// be called on this object after the configuration properties have
+        /// been set. Until <see cref="ActivateOptions"/> is called this
+        /// object is in an undefined state and must not be used. 
+        /// </para>
+        /// <para>
+        /// If any of the configuration properties are modified then 
+        /// <see cref="ActivateOptions"/> must be called again.
+        /// </para>
+        /// </remarks>
+        public override void ActivateOptions()
+        {
+            base.ActivateOptions();
+
+            this.Collection = GetCollection();
+        }
+
+        /// <summary>
+        /// Appends a logging event to Mongo
+        /// </summary>
+        /// <param name="loggingEvent">The logging event</param>
+		protected override void Append(LoggingEvent loggingEvent)
+        {
+            BsonDocument record = BuildBsonDocument(loggingEvent);
+            Collection.Insert(record);
+        }
+
+        /// <summary>
+        /// Inserts the events into the database.
+        /// </summary>
+        /// <param name="events">The events to insert into the database.</param>
+        /// <remarks>
+        /// <para>
+        /// Insert all the events specified in the <paramref name="events"/>
+        /// array into the database.
+        /// </para>
+        /// </remarks>
+        protected override void SendBuffer(LoggingEvent[] events)
+        {
+            foreach (LoggingEvent logEvent in events)
+            {
+                Append(logEvent);
+            }
+        }
+
+        /// <summary>
+        /// Gets the Mongo collection that the logs will be written to. If one isn't specified 
+        /// in the configuration then it defaults to 'logs'.
+        /// </summary>
+        /// <returns>The Mongo collection</returns>
+		protected virtual MongoCollection GetCollection()
+		{
+			MongoDatabase db = GetDatabase();
+			MongoCollection collection = db.GetCollection(CollectionName ?? "logs");
+			return collection;
+		}
+
+        /// <summary>
+        /// Gets the connection string by name or by using the connection string property if unavailable.
+        /// </summary>
+        /// <returns>The connection string</returns>
+		protected virtual String GetConnectionString()
+		{
+			ConnectionStringSettings connectionStringSetting = ConfigurationManager.ConnectionStrings[ConnectionStringName];
+			return connectionStringSetting != null ? connectionStringSetting.ConnectionString : this.ConnectionString;
+		}
+
+        /// <summary>
+        /// Gets the Mongo database based on the connection string. IF the database name isn't 
+        /// present in the connection string it defaults to 'log4net'.
+        /// </summary>
+        /// <returns>The Mongo database</returns>
+		protected virtual MongoDatabase GetDatabase()
+		{
+            String connectionString = GetConnectionString();
+
+			MongoUrl url = MongoUrl.Create(connectionString);
+            MongoClient client = new MongoClient(url);
+			MongoServer conn = client.GetServer();
+
+			MongoDatabase db = conn.GetDatabase(url.DatabaseName ?? "log4net");
+			return db;
+		}
+
+        /// <summary>
+        /// Builds the BSON document to send to Mongo from the log4net LoggingEvent.
+        /// </summary>
+        /// <param name="log">The logging event</param>
+        /// <returns>The BSON document</returns>
+		private BsonDocument BuildBsonDocument(LoggingEvent log)
+		{
+            BsonDocument doc = new BsonDocument();
+
+            foreach (MongoAppenderField field in this.fields)
+            {
+                try
+                {
+                    if (field.Layout != null)
+                    {
+                        object value = field.Layout.Format(log);
+                        BsonValue bsonValue = value as BsonValue ?? BsonValue.Create(value);
+                        doc.Add(field.Name, bsonValue);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.Error.WriteLine(ex.ToString());
+                }
+            }
+
+		    return doc;
+		}
+	}
+}
